df49c03a00e75afbcd0e7542e09b6ee684a93539
bainbrid <bainbrid@users.noreply.github.com>
2020-05-22 15:59:38 +0200

    update

--- run/ntuplizer_cfg.py
+++ run/ntuplizer_cfg.py
@@ -1,14 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 from FWCore.ParameterSet.VarParsing import VarParsing
-options = VarParsing('python')
+options = VarParsing ('python')
+options.setDefault('inputFiles','file:input.root')
+options.setDefault('outputFile','output.root')
 options.setDefault('maxEvents',-1)
 options.register('skipEvents',0,VarParsing.multiplicity.singleton,VarParsing.varType.int,"")
 options.register('useAOD',False,VarParsing.multiplicity.singleton,VarParsing.varType.bool,"")
 options.register('addSkim',False,VarParsing.multiplicity.singleton,VarParsing.varType.bool,"")
 options.parseArguments()
 
-process = cms.Process('TEST')
+process = cms.Process('TEST') 
 
 # AOD: 9558 + 6230 + 1661 = 17449 events
 # MINOAOD: 17449 events
@@ -23,35 +25,48 @@ process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(default_files),
     secondaryFileNames = cms.untracked.vstring()
+    skipEvents = cms.untracked.uint32(options.skipEvents),
     )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(options.maxEvents)
     )
 
-output_file = 'output_fromAOD_ntuple.root' if options.useAOD else 'output_fromMINIAOD_ntuple.root'
+process.options = cms.untracked.PSet(
+    numberOfThreads=cms.untracked.uint32(1),
+    numberOfStreams=cms.untracked.uint32(0),
+)
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v15', '')
 
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string(output_file)
+    fileName = cms.string(options.outputFile)
     )
 
 process.output = cms.OutputModule(
     "PoolOutputModule",
-    fileName = cms.untracked.string(output_file.replace('ntuple','skim')),
+    fileName = cms.untracked.string('output_filtered.root'),
     SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('ntuplizer_path'))
 )
 
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-if options.useAOD : switchOnVIDElectronIdProducer(process,DataFormat.AOD)
-else :           switchOnVIDElectronIdProducer(process,DataFormat.MiniAOD)
-for idmod in ['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V2_cff'] :
+if options.useAOD is True : switchOnVIDElectronIdProducer(process,DataFormat.AOD)
+else :                      switchOnVIDElectronIdProducer(process,DataFormat.MiniAOD)
+for idmod in [
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V2_cff',
+    ] : 
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
 process.ntuplizer_seq = cms.Sequence()
 
 process.load('RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cff')
-if not options.useAOD : 
+if options.useAOD is False : 
     process.lowPtGsfElectronID.electrons = 'slimmedLowPtElectrons'
     process.lowPtGsfElectronID.rho = 'fixedGridRhoFastjetAll'
 process.ntuplizer_seq *= process.lowPtGsfElectronID
