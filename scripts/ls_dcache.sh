12cabc81507ed4a5f91efae0e4b36c2f2ee1f755
bainbrid <bainbrid@users.noreply.github.com>
2020-09-25 08:51:07 +0200

    useful scripts

---
+++ scripts/ls_dcache.sh
@@ -0,0 +1,12 @@
+# to be run at IC
+cmstools
+voms
+
+# miniAOD
+#DIR=store/user/bainbrid/lowpteleid/BuToKJpsi_Toee_MuFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/crab_lowpteleid/190328_152903/0000
+
+# ntuples
+#DIR=store/user/bainbrid/lowpteleid/CRAB_UserFiles/crab_lowpteleid/190329_121337/0000
+
+# ls command
+uberftp sedsk53.grid.hep.ph.ic.ac.uk 'ls DIR' > pippo 
