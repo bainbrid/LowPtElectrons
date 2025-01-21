1fa77373c5965917f087011bfa556eabac54672f
bainbrid <bainbrid@users.noreply.github.com>
2025-01-21 14:20:40 +0100

    triv

--- run/test.sh
+++ run/test.sh
@@ -1,5 +1,5 @@
-#cmsRun ntuplizer_test_cfg.py
-#cmsRun ntuplizer_test_cfg.py maxEvents=100 >& pippo1 &
-#cmsRun ntuplizer_test_cfg.py maxEvents=100 simple=1 >& pippo2 &
+#cmsRun ntuplizer_mc_cfg.py
+#cmsRun ntuplizer_mc_cfg.py maxEvents=100 >& pippo1 &
+#cmsRun ntuplizer_mc_cfg.py maxEvents=100 simple=1 >& pippo2 &
 #cmsRun ntuplizer_data_cfg.py maxEvents=100 >& pippo3 &
 #cmsRun ntuplizer_data_cfg.py maxEvents=100 simple=1 >& pippo4 &
